8e4f6fe88767d562d760ca5ee6b3155017a827e0
Janelle McGill <you@example.com>
2021-06-11 16:50:06 -0400

    update game set up

--- blackjack.rb
+++ blackjack.rb
@@ -2,23 +2,62 @@
 
 puts "Welcome to ruby blackjack"
 
+################### SETTING UP ###################
+
+#Creates a new player
 class Player
     attr_accessor :name
     attr_accessor :bankroll
     attr_accessor :hand
+    attr_accessor :total
 
     def initialize 
         @name = "player"
         @bankroll = 100
         @hand = []
+        @total = total
     end
+end
+
+#Creates a deck and 52 cards
+class Deck
+    def initialize
+        @faces = [*2..10, "J", "Q", "K", "A"]
+        @suits = ["clurbs", "spurds", "hurts", "dimmys"]
+        @cards = []
+
+        #loop through each face
+        @faces.each do |face|
+            if face.class == Integer
+                value = face 
+            #if face is A (aka ace) the value is 11
+            elsif face == "A"
+                value = 11
+            #all other faces (J, Q, K, A) are worth 10
+            else
+                value = 10
+            end
+        @suits.each do |suit|
+        @cards << Card.new(face, suit, value)
+        end
+    end
+    #shuffle cards
+    @cards.shuffle!
+    end
+
+
+
 end
 
 class Card
     attr_accessor :value
+    attr_accessor :face
+    attr_accessor :suit
 
     def initialize value 
         @value = value
+        @face = face
+        @suit = suit
     end
 end
 
@@ -31,3 +70,5 @@ puts the_house.name
 puts the_house.bankroll
 
 deck []
+
+################### PLAYING THE GAME ###################
