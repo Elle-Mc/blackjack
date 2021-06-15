51526d87c33c81d3ea40e25a98baf031ba83580f
Janelle McGill <you@example.com>
2021-06-15 09:11:30 -0400

    update game

--- blackjack.rb
+++ blackjack.rb
@@ -45,7 +45,9 @@ class Deck
     @cards.shuffle!
     end
 
-
+    def deal (num, player)
+        num.times {@cards.shift.generate_card(player)}
+    end
 
 end
 
@@ -59,7 +61,19 @@ class Card
         @face = face
         @suit = suit
     end
+
+    #generate a card from the deck
+    def generate_card (player)
+        new_card = Card.new face, suit, value
+        player.hand << new_card
+        #update total for player
+        player.total = player.total + new_card.value
+    end
+
 end
+
+
+################### CREATES PLAYER AND COMPUTER ###################
 
 human = Player.new "Human", 1000
 the_house = Player.new "The hizzy (or dealerdood)", 10000
@@ -72,3 +86,4 @@ puts the_house.bankroll
 deck []
 
 ################### PLAYING THE GAME ###################
+
